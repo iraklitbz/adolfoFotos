e6b5d88500fb162a23d0f392f7888bb02eba7d34
iraklitbz <iraklitbz@gmail.com>
2017-09-22 09:22:36 +0200

    modificado: src/components/pages/Actors.vue 	modificado: src/components/pages/People.vue

--- src/components/pages/Actors.vue
+++ src/components/pages/Actors.vue
@@ -2,7 +2,7 @@
 <div class="galleryEditorials">
     <div class="column">
        <ul class="galery-horiz">
-           <li v-for="foto in fotosLef" :key="foto.id"> 
+           <li v-for="foto in fotosLef"> 
                <img :src="'/src/images/actores/' + foto.pic.url + '.jpg'" > 
                <h2>{{foto.pic.name}}</h2>
            </li>
@@ -35,7 +35,148 @@ import _ from 'lodash';
 import { db } from './../../firebase';
 export default {
   data(){
-     
+      let randoM = {
+            fotosRig: [
+                        { pic: {
+                            url: 'ALBERTORODRIGUEZ',
+                            name: 'Alberto Rodriguez'
+                        } },
+                        { pic: {
+                            url: 'ALEXGARCIA',
+                            name: 'Alex García'
+                        } },
+                        { pic: {
+                            url: 'ALEXGARCIA2',
+                            name: 'Alex García'
+                        } },
+                        { pic: {
+                            url: 'AMAIASALAMANCA',
+                            name: 'Amaia Salamanca'
+                        } },
+                        { pic: {
+                            url: 'ANTONIODELATORRE',
+                            name: 'Antonio De La Torre'
+                        } },
+                        { pic: {
+                            url: 'ANTONIODELATORRE2',
+                            name: 'Antonio De La Torre'
+                        } },
+                        { pic: {
+                            url: 'MARTINRIVAS5',
+                            name: 'Martín Rivas'
+                        } },
+                        { pic: {
+                            url: 'MEGANMONTANER',
+                            name: 'Megan Montera'
+                        } },
+                        { pic: {
+                            url: 'MEGANMONTANER2',
+                            name: 'Megan Montera'
+                        } },
+                        { pic: {
+                            url: 'PATRICIACONDE',
+                            name: 'Patricia Conde'
+                        } },
+                        { pic: {
+                            url: 'URSULACORBERO',
+                            name: 'Ursula Corbero'
+                        } }
+                        
+                ],
+
+                fotosMidd: [
+                        { pic: {
+                            url: 'ANTONIORESINES',
+                            name: 'Antonio Resines'
+                        } },
+                        { pic: {
+                            url: 'ASTRIDBERGESFRISBEY2',
+                            name: 'Àstrid Bergès-Frisbey'
+                        } },
+                        { pic: {
+                            url: 'DANIELGUZMAN',
+                            name: 'Daniel Guzman'
+                        } },
+                        { pic: {
+                            url: 'JOSECORONADO',
+                            name: 'José Cornado'
+                        } },
+                        { pic: {
+                            url: 'JOSEMOTA',
+                            name: 'José Mota'
+                        } },
+                        { pic: {
+                            url: 'JOSEMOTA2',
+                            name: 'José Mota'
+                        } },
+                        { pic: {
+                            url: 'MIGUELANGELSILVESTRE',
+                            name: 'Miguel Ángel Silvestre'
+                        } },
+                        { pic: {
+                            url: 'MIGUELANGELSILVESTRE2',
+                            name: 'Miguel Ángel Silvestre'
+                        } },
+                        { pic: {
+                            url: 'MIGUELANGELSILVESTRE3',
+                            name: 'Miguel Ángel Silvestre'
+                        } },
+                        { pic: {
+                            url: 'PATRICIACONDE2',
+                            name: 'Patricia Conde'
+                        } },
+                        { pic: {
+                            url: 'YONGONZALEZ2',
+                            name: 'Yon Gonzalez'
+                        } }
+                ],
+                fotosLef: [
+                    { pic: {
+                        url: 'LETICIADOLERA',
+                        name: 'Leticia Dolera'
+                    } },
+                    { pic: {
+                        url: 'MANUELAVELLES',
+                        name: 'Manuel Avelles'
+                    } },             
+                    { pic: {
+                        url: 'MARIANALVAREZ',
+                        name: 'Maria Nalvarez'
+                    } },  
+                    { pic: {
+                        url: 'MARIOCASAS',
+                        name: 'Mario Casas'
+                    } },
+                    { pic: {
+                        url: 'MARIOCASAS2',
+                        name: 'Mario Casas'
+                    } },              
+                    { pic: {
+                        url: 'MARTINRIVAS2',
+                        name: 'Martín Rivas'
+                    } },
+                    { pic: {
+                        url: 'MIRIAMGIOVANELLI',
+                        name: 'Miriam Giovanelli'
+                    } },
+                    { pic: {
+                        url: 'OONACHAPLIN1',
+                        name: 'Oona Chaplin'
+                    } },
+                    { pic: {
+                        url: 'OONACHAPLIN3',
+                        name: 'Oona Chaplin'
+                    } },
+                    { pic: {
+                        url: 'SANTIMILLAN2',
+                        name: 'Santi Millan'
+                    } },
+                    { pic: {
+                        url: 'YONGONZALEZ3',
+                        name: 'Yon Gonzalez'
+                    } }
+                ]
+             }
 
 
     randoM.fotosRig = _.shuffle(randoM.fotosRig);
@@ -44,6 +185,8 @@ export default {
       
       return randoM;
   },
+
+  /*
 
   created(){
       let actors = db.ref('/Actors').on('value', snapshot => this.cargarArray(snapshot.val()))
@@ -54,7 +197,7 @@ export default {
          console.log(actors)
       }
   }
- 
+ */
  
 }
 </script>
