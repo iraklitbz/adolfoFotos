4e9e26d3bc9101994d800a4c2b136d962e754e71
iraklitbz <iraklitbz@gmail.com>
2017-09-26 11:26:05 +0200

    modificado: index.html 	modificado: src/components/pages/Contacto.vue

--- src/components/pages/Contacto.vue
+++ src/components/pages/Contacto.vue
@@ -1,8 +1,7 @@
 <template>
      <div class="contacto">
-         <h2>Contacto</h2>
+         <p><i class="fa fa-envelope-o"></i><a class="activ" href="mailto:info@adolfocallejo.com"> info@adolfocallejo.com </a></p>
          <ul class="contact-me">
-            <li><i class="fa fa-envelope-o"></i><a class="activ" href="mailto:info@adolfocallejo.com"> info@adolfocallejo.com </a></li>
             <li class="tel"><i class="fa fa-whatsapp"></i>658 595 141</li>
             <li class="insta"><i class="fa fa-instagram"></i> <a href="https://www.instagram.com/adolfo_callejo/">adolfo_callejo</a></li>
          </ul>
@@ -23,31 +22,53 @@ export default {
 .contacto {
   max-width: 400px;
   margin: 20px auto;
+   i {
+            margin-right: 10px;
+             color: #333;
+             font-size: 24px;
+        }
+   p {
+       text-align: center;
+           font-weight: 300;
+       a {
+            font-family: 'Lato', sans-serif;
+            padding: 10px 0px;
+            font-size: 24px;
+            color: #D32C2E;
+            text-decoration: none;
+        }
+   }
 
     h2 {
         font-family: 'Lato', sans-serif!important;
         margin-bottom: 20px;
         font-size: 26px;
         text-align: center;
-        font-weight: 100!important;
         color: #5b5b5b
     }
 }
 
     .contact-me {
+        text-align: center;
+        margin-top: 15px;
         li {
             font-family: 'Lato', sans-serif;
         padding: 10px 0px;
+                  font-weight: 300;
         font-size: 24px;
-         color: #5b5b5b;
+         color: #333;
+         display: inline-block;
+         &:first-child{
+             margin-right: 20px;
+         }
+  
         i {
             margin-right: 10px;
-             color: #5b5b5b
+             color: #333
         }
         a {
-            font-family: 'Lato', sans-serif;
             text-decoration: none;
-             color: #5b5b5b;
+             color: #333;
             transition: color 0.2s linear;
             &:hover {
                 color: #D32C2E
@@ -70,6 +91,14 @@ export default {
             font-size: 20px;
             }
         }
+            .contact-me {
+                text-align: center;
+                margin-top: 15px;
+                li {
+                display:block;
+
+        }
+     }
      }
 
 </style>
